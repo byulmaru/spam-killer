a5ce0825a41ba55c8a69b5c431ce44f8ddc06d69
robin* <robin_maki@planet.moe>
2024-02-17 15:43:52 +0900

    최소 멘션 2개로 축소

--- bot.ts
+++ bot.ts
@@ -14,10 +14,9 @@ const streaming = createStreamingAPIClient({
 for await(const event of streaming.public.subscribe()) {
   if(event.event === 'update' && !event.payload.inReplyToId) {
     if(event.payload.account.acct.includes('@') && event.payload.account.username === event.payload.account.displayName) {
-      if(event.payload.mentions.length >= 5 && dayjs(event.payload.account.createdAt).isSame(dayjs(), 'day')) {
+      if(event.payload.mentions.length >= 2 && dayjs(event.payload.account.createdAt).isSame(dayjs(), 'day')) {
         if(/^[a-z0-9]{10}$/.test(event.payload.account.username)) {
           console.log(`${event.payload.account.acct} is a bot`);
-          event.payload.account.
           rest.v1.admin.accounts.$select(event.payload.account.id).action.create({
             type: 'suspend',
             text: 'Mention Bot (Auto)'
